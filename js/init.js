14b6126411e2906a9291e2dbacdd7d108620a172
edwincarranza <87099939+edwincarranza@users.noreply.github.com>
2021-07-20 15:08:27 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyDD8xTz_V7NQnasjZAkMSZprnPyjEX6jvo",
+    authDomain: "chatedwincarranza.firebaseapp.com",
+    projectId: "chatedwincarranza",
+    storageBucket: "chatedwincarranza.appspot.com",
+    messagingSenderId: "849448005434",
+    appId: "1:849448005434:web:03cc35db51ebdad9cc6a03",
+    measurementId: "G-P1M3YFMH0Q"
+    });
