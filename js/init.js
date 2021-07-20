d83b059e46c541a93234610e9d7768f5d0eea41a
edwincarranza <87099939+edwincarranza@users.noreply.github.com>
2021-07-20 15:31:29 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,9 +1,9 @@
 firebase.initializeApp({
-  apiKey: "AIzaSyDD8xTz_V7NQnasjZAkMSZprnPyjEX6jvo",
-    authDomain: "chatedwincarranza.firebaseapp.com",
-    projectId: "chatedwincarranza",
-    storageBucket: "chatedwincarranza.appspot.com",
-    messagingSenderId: "849448005434",
-    appId: "1:849448005434:web:03cc35db51ebdad9cc6a03",
-    measurementId: "G-P1M3YFMH0Q"
+  apiKey: "AIzaSyB2JQgHXo_jZhqw9NGc4cvLjSykb-HvNYA",
+    authDomain: "chatedca-718c7.firebaseapp.com",
+    projectId: "chatedca-718c7",
+    storageBucket: "chatedca-718c7.appspot.com",
+    messagingSenderId: "868114704655",
+    appId: "1:868114704655:web:f568a04375c618da5d767a",
+    measurementId: "G-0Y2HLDK3XQ"
     });
